19dcbf245e47ec6cf9365b065d50b6adde36a2b2
Arun <arun5320@users.noreply.github.com>
2023-05-25 13:06:32 +0530

    Updated

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function timer() {
-    var d = new Date();
-    var hours = d.getHours();
-    var min = d.getMinutes();
-    var Sec = d.getSeconds();
-
-   var date= document.getElementsByClassName("container");
-   date.innerHTML ="Time"+":"+ hours+":"+min+":"+Sec;
-}
-setInterval(timer, 1000);
-
 var loader = document.getElementById("preloader");
-    window.addEventListener("load", function(){
-     loader.style.display = "none";
-    })
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
